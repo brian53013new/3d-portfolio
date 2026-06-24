1153ee5bf7f53caeec52d7a71d770d16c1b22897
brian53013new <brian53013.new@gmail.com>
2026-06-24 12:45:33 +0800

    fix: update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3d-portfolio/', // Matches the GitHub repository name
+  base: '/Brian-Code-Official-website/', // Matches the GitHub repository name
 })
